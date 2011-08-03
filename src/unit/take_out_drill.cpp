822943259f09b9b46ef30d2c2a41614c2847d12d
CiliesJ <CiliesJ@43b2f215-69ce-7cdb-bcbe-f39a4413677e>
2011-08-03 06:44:27 +0000

    Take out drill animations done.

--- src/unit/take_out_drill.cpp
+++ src/unit/take_out_drill.cpp
@@ -3,12 +3,115 @@
 
 void bClassUnit::take_out_drill() //Take out its drill.
 {
-	cout << "Taking out drill.\n";
+	cout << "Taking out drill.\n"; //Debugging output
 
 	if(Map[mine_tile_id].wx < wx && takeout_drill_left)
 	{
 		//PROGRESS THE ANIMATION AS NEEDED.
 		cout << "Blarg. Tile to left and has takeout drill left animation.\n"; //Debugging output.
+
+		active_animation = takeout_drill_left;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_left->current_frame < takeout_drill_left->num_frames - 1)
+		{
+			cout << "Progressing animation.\n";
+
+			takeout_drill_left->proceed_animation();
+
+			take_out_drill_progress = 0;
+		}
+		take_out_drill_progress++;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_left->current_frame >= takeout_drill_left->num_frames - 1)
+		{
+			//THE DRILL IS OUT.
+			cout << "Yay. Drill out. Now go slap yourself silly. Er, I mean, watch your raider drill das wall.\n"; //Debugging output.
+
+			take_out_drill_progress = 0; //Reset this.
+			drill_out = true; //Let the game know this unit has its drill out.
+			takeout_drill_left->current_frame = 0;
+		}
+	}
+	else if(Map[mine_tile_id].wx > wx && takeout_drill_right)
+	{
+		//PROGRESS THE ANIMATION AS NEEDED.
+		cout << "Blarg. Tile to right and has takeout drill right animation.\n"; //Debugging output.
+
+		active_animation = takeout_drill_right;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_right->current_frame < takeout_drill_right->num_frames - 1)
+		{
+			cout << "Progressing animation.\n";
+
+			takeout_drill_right->proceed_animation();
+
+			take_out_drill_progress = 0;
+		}
+		take_out_drill_progress++;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_right->current_frame >= takeout_drill_right->num_frames - 1)
+		{
+			//THE DRILL IS OUT.
+			cout << "Yay. Drill out. Now go slap yourself silly. Er, I mean, watch your raider drill das wall.\n"; //Debugging output.
+
+			take_out_drill_progress = 0; //Reset this.
+			drill_out = true; //Let the game know this unit has its drill out.
+			takeout_drill_right->current_frame = 0;
+		}
+	}
+	else if(Map[mine_tile_id].wy < wy && takeout_drill_up)
+	{
+		//PROGRESS THE ANIMATION AS NEEDED.
+		cout << "Blarg. Tile to north and has takeout drill up animation.\n"; //Debugging output.
+
+		active_animation = takeout_drill_up;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_up->current_frame < takeout_drill_up->num_frames - 1)
+		{
+			cout << "Progressing animation.\n";
+
+			takeout_drill_up->proceed_animation();
+
+			take_out_drill_progress = 0;
+		}
+		take_out_drill_progress++;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_up->current_frame >= takeout_drill_up->num_frames - 1)
+		{
+			//THE DRILL IS OUT.
+			cout << "Yay. Drill out. Now go slap yourself silly. Er, I mean, watch your raider drill das wall.\n"; //Debugging output.
+
+			take_out_drill_progress = 0; //Reset this.
+			drill_out = true; //Let the game know this unit has its drill out.
+			takeout_drill_up->current_frame = 0;
+		}
+	}
+	else if(Map[mine_tile_id].wy > wy && takeout_drill_down)
+	{
+		//PROGRESS THE ANIMATION AS NEEDED.
+		cout << "Blarg. Tile to south and has takeout drill down animation.\n"; //Debugging output.
+
+		active_animation = takeout_drill_down;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_down->current_frame < takeout_drill_down->num_frames - 1)
+		{
+			cout << "Progressing animation.\n";
+
+			takeout_drill_down->proceed_animation();
+
+			take_out_drill_progress = 0;
+		}
+		take_out_drill_progress++;
+
+		if(take_out_drill_progress >= take_out_drill_time && takeout_drill_down->current_frame >= takeout_drill_down->num_frames - 1)
+		{
+			//THE DRILL IS OUT.
+			cout << "Yay. Drill out. Now go slap yourself silly. Er, I mean, watch your raider drill das wall.\n"; //Debugging output.
+
+			take_out_drill_progress = 0; //Reset this.
+			drill_out = true; //Let the game know this unit has its drill out.
+			takeout_drill_down->current_frame = 0;
+		}
 	}
 	else
 	{
@@ -20,22 +123,6 @@ void bClassUnit::take_out_drill() //Take out its drill.
 			take_out_drill_progress = 0; //Reset this.
 			drill_out = true; //Let the game know this unit has its drill out.
 		}
-
 		take_out_drill_progress++;
 	}
-
-			/* //Check if it has the animation.
-			if(Map[unitlist[i].mine_tile_id].wx < unitlist[i].wx && unitlist[i].takeout_drill_left) //Tile is to left and has animation?
-			{
-				//PROGRESS THE ANIMATION AS NEEDED>
-				cout << "Blarg. Tile to left and has takeout drill left animation.\n";
-			}
-			else
-			{
-				if(unitlist[i].take_out_drill_progress >= unitlist[i].take_out_drill_time)
-				{
-					//THE DRILL IS OUT.
-					cout << "Yay. Drill out. Now go slap yourself silly. Er, I mean, watch your raider drill das wall.\n";
-				}
-			}*/
 }
